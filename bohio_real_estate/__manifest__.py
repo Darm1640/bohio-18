d9404ba48b19ff5de21210063871fd010d0e8a3b
Darm1640 <93557532+Darm1640@users.noreply.github.com>
2025-10-05 16:16:26 -0500

    Update __manifest__.py

--- bohio_real_estate/__manifest__.py
+++ bohio_real_estate/__manifest__.py
@@ -24,6 +24,7 @@
         'website',
         'helpdesk',
         'portal',
+        'mail',
         'account_loans',  # Odoo Enterprise - Gestión de préstamos
     ],
     'external_dependencies': {
